e67c2e1a069cb68713bc4c285bebaeb3c6b80b3a
BashithR <bkratnaweera04@gmail.com>
2026-05-27 22:01:31 +0530

    feat: add loading component for Standup Dashboard

---
+++ src/app/tasks/standup-dashboard/[projectId]/loading.tsx
@@ -0,0 +1,5 @@
+import { ContentLoader } from '@/components/ui/ContentLoader'
+
+export default function StandupProjectLoading() {
+  return <ContentLoader message="Loading standup project..." />
+}
