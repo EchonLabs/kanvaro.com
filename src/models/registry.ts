5613e8afd36519bd49114bc11f08eb015f4b084f
Lakith Garusingarachchi <Lakith.20231573@iit.ac.lk>
2026-06-02 13:40:07 +0530

    Revert "Merge pull request #843 from EchonLabs/improvments/AI-Feature-report-gen"
    This reverts commit 0004e5314a12a3aaa584420702c4d23cbfd9ea01, reversing
    changes made to bf782d16c223401247ec2682d8fc664b6c07dad1.

--- src/models/registry.ts
+++ src/models/registry.ts
@@ -39,8 +39,6 @@ import './Counter'
 import './Notification'
 import './StandupSchedule'
 import './StandupSummary'
-import './AIProjectReport'
-import './StandupCronJob'
 
 // Export all models for convenience
 export * from './index'
